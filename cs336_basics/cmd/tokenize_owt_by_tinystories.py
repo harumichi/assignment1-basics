5b1ad1eef706d1f8bf72d7682fe02cd4b042b7c2
Yokoyama Harumichi <10958795+harumichi@users.noreply.github.com>
2025-10-02 18:48:18 +0900

    tokenize owt by tokenizer trained by tinystories

---
+++ cs336_basics/cmd/tokenize_owt_by_tinystories.py
@@ -0,0 +1,32 @@
+import time
+import logging
+import os
+import numpy as np
+
+import cs336_basics  # triggers logging configuration in package __init__
+from cs336_basics.bpe import Tokenizer
+
+logger = logging.getLogger(__name__)
+
+work_dir = "output/bpe/tinystories"
+names = ["owt_valid"]
+
+begin_time = time.time()
+tokenizer = Tokenizer.from_files(
+    vocab_filepath=os.path.join(work_dir, "vocab.pkl"),
+    merges_filepath=os.path.join(work_dir, "merges.pkl"),
+    special_tokens=["<|endoftext|>"],
+)
+for name in names:
+    logger.info("Tokenizing %s...", name)
+    input_path = f"data/{name}.txt"
+    suffix = "_by_tinystories"
+    output_path = f"data/{name}{suffix}.npy"
+    with open(input_path, "r", encoding="utf-8") as fp:
+        logger.info("File size: %.2f bytes", os.path.getsize(input_path))
+        arr = np.fromiter(tokenizer.encode_iterable(fp), dtype=np.uint16)
+    logger.info("Number of tokens: %d", len(arr))
+    np.save(output_path, arr)
+end_time = time.time()
+
+logger.info("Tokenization elapsed time: %.2f s", end_time - begin_time)
