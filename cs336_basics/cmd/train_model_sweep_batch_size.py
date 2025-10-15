dc927363fcd1f813d4d8e87f04724c6753ed2ae3
Yokoyama Harumichi <10958795+harumichi@users.noreply.github.com>
2025-10-16 06:47:12 +0900

    fix

--- cs336_basics/cmd/train_model_sweep_batch_size.py
+++ cs336_basics/cmd/train_model_sweep_batch_size.py
@@ -12,7 +12,8 @@ valid_path = "data/TinyStoriesV2-GPT4-valid.npy"
 total_tokens = 327_680_000
 context_length = 256
 
-batch_size_list = [8, 32, 128]
+# batch_size_list = [8, 32, 128]
+batch_size_list = [16, 64]
 for batch_size in batch_size_list:
     logger.info(f"Training with batch_size: {batch_size}")
     run_name = 'tinystories/sweep_batch_size_' + str(batch_size)
